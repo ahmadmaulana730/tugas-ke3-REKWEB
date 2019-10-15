92af29e465caa8c290936725f9799943e92fdb3b
Ahmad maulana <amkomarudin7@gmail.com>
2019-10-16 05:38:19 +0700

    fungsi pagenation finish

--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,9 +1,21 @@
 <div class="container">
+    <h2 class="mt-3">List Peoples</h2>
+
+    <div class="row">
+        <div class="col-md-4">
+            <form action="<?= base_url('peoples'); ?>" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="keyword" placeholder="Search Keyword" autocomplete="off" autofocus>
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary" type="submit" name="submit">Searching</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-18">
-
-            <h2 class="mt-3">List Peoples</h2>
-
             <table class="table">
                 <thead>
                     <tr>
